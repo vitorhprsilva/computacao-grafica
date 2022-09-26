e74607b638bb93186562f740ea561d968f2555ac
vitor <vitorhprsilva@gmail.com>
2022-09-26 20:18:49 -0300

    feat: file

---
+++ figura1.d
@@ -0,0 +1,25 @@
+#-----------#
+# Figura 1 #
+#-----------#
+
+#=========
+| POINTS |
+=========#
+4 # number of points #
+
+# Nodes which define the boundary #
+0:  -1.0  -1.0    0.05    1
+1:   1.0  -1.0    0.05    1
+2:   1.0   1.0    0.05    1
+3:  -1.0   1.0    0.05    1
+
+#===========
+| SEGMENTS |
+===========#
+4 # Number of segments #
+
+# Boundary segments #
+0:  0  1    1
+1:  1  2    1
+2:  2  3    1
+3:  3  0    1
